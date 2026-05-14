e233fa89f3dc6abcbe8786d4cc76b07259331458
Adrian Gizzi <adriangizzi@MacBook-Air-de-Adrian.local>
2026-05-14 13:27:07 -0300

    debug respuesta meta

--- app/main.py
+++ app/main.py
@@ -1,9 +1,15 @@
 from fastapi import FastAPI, Request
 from fastapi.responses import PlainTextResponse
+from dotenv import load_dotenv
+import os
+import requests
+
+load_dotenv()
 
 app = FastAPI()
 
-VERIFY_TOKEN = "bot_automotriz_zabaleoMotors_2026"
+VERIFY_TOKEN = os.getenv("VERIFY_TOKEN")
+PAGE_ACCESS_TOKEN = os.getenv("PAGE_ACCESS_TOKEN")
 
 @app.get("/")
 async def home():
@@ -31,4 +37,51 @@ async def receive_message(request: Request):
     print("Mensaje recibido:")
     print(body)
 
+    try:
+
+        entry = body["entry"][0]
+        messaging = entry["messaging"][0]
+
+        sender_id = messaging["sender"]["id"]
+
+        if "message" in messaging:
+
+            user_message = messaging["message"].get("text", "")
+
+            send_message(
+                sender_id,
+                f"Hola 👋 recibimos tu mensaje: {user_message}"
+            )
+
+    except Exception as e:
+        print("ERROR:")
+        print(e)
+
     return {"status": "ok"}
+
+def send_message(recipient_id, message_text):
+
+    url = f"https://graph.facebook.com/v19.0/me/messages?access_token={PAGE_ACCESS_TOKEN}"
+
+    headers = {
+        "Content-Type": "application/json"
+    }
+
+    payload = {
+        "recipient": {
+            "id": recipient_id
+        },
+        "message": {
+            "text": message_text
+        }
+    }
+
+    response = requests.post(
+        url,
+        headers=headers,
+        json=payload
+    )
+
+    print("RESPUESTA META:")
+    print(response.status_code)
+    print(response.text)
