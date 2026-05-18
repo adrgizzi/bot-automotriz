624e9f405ee5fb2ad8aa40929aa86c3a9392b9d5
Adrian Gizzi <adriangizzi@MacBook-Air-de-Adrian.local>
2026-05-18 12:45:22 -0300

    cambios responses

--- app/services/responses.py
+++ app/services/responses.py
@@ -3,6 +3,12 @@ from app.services.sheets import buscar_autos
 def generar_respuesta(texto):
 
     autos = buscar_autos(texto)
+    
+    if "hola" in texto or "buenas" in texto:
+        return (
+        "Hola 👋 Soy el asistente virtual de Zabaleo Motors 🚗\n"
+        "¿Qué vehículo estás buscando?"
+    )
 
     if len(autos) > 0:
 
@@ -16,10 +22,14 @@ def generar_respuesta(texto):
             f"💵 Precio: USD {auto['precio']}\n"
             f"⚙️ Transmisión: {auto['transmisión']}\n"
             f"⛽ Combustible: {auto['combustible']}\n"
-            f"🛣️ KM: {auto['km']}\n\n" # con el doble separa modelo a modelo 
+            f"🛣️ KM: {auto['km']}\n" 
+            f"🏢 Agencia: {auto['agencia']}\n\n"# con el doble separa modelo a modelo 
             
 )
-
+            respuesta += (
+             "\n👉 ¿Cuál te interesa más?\n"
+            "Puedo ayudarte con financiación, permutas o más fotos."
+)
         return respuesta
 
     return "No encontré vehículos con esa búsqueda 😕"
