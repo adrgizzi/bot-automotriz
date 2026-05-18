ef9880c71fc91e3cb5f304bb756591c79d797bfe
Adrian Gizzi <adriangizzi@MacBook-Air-de-Adrian.local>
2026-05-18 12:38:40 -0300

    cambios responses

--- app/services/responses.py
+++ app/services/responses.py
@@ -11,14 +11,14 @@ def generar_respuesta(texto):
         for _, auto in autos.iterrows():
 
             respuesta += (
-                f"{auto['marca']} "
-                f"{auto['modelo']} "
-                f"{auto['año']} - "
-                f"{auto['precio']} - "
-                f"Transmision : {auto['transmisión']} - "
-                f"Combustible : {auto['combustible']} - "
-                f"Kilometros : {auto['km']}\n"
-            )
+             f"🚗 {auto['marca']} {auto['modelo']}\n"
+            f"📅 Año: {auto['año']}\n"
+            f"💵 Precio: USD {auto['precio']}\n"
+            f"⚙️ Transmisión: {auto['transmisión']}\n"
+            f"⛽ Combustible: {auto['combustible']}\n"
+            f"🛣️ KM: {auto['km']}\n\n" # con el doble separa modelo a modelo 
+            
+)
 
         return respuesta
 
