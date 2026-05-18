1b25c55cdee06ebda54746f1ed5c9f9f810d7485
Adrian Gizzi <adriangizzi@MacBook-Air-de-Adrian.local>
2026-05-18 10:58:37 -0300

    fix: add sheets.py locally

--- app/services/sheets.py
+++ app/services/sheets.py
@@ -3,11 +3,6 @@ import pandas as pd
 SHEET_URL = "https://docs.google.com/spreadsheets/d/1fEmcM4fzV2TwmzyZVqF9yS2mxH305CarUKURpC4t1xo/export?format=csv"
 
 def buscar_autos(query):
-
     df = pd.read_csv(SHEET_URL)
-
-    resultados = df[
-        df["modelo"].str.lower().str.contains(query.lower(), na=False)
-    ]
-
-    return resultados
+    mascara = df.apply(lambda row: row.astype(str).str.contains(query, case=False).any(), axis=1)
+    return df[mascara]
