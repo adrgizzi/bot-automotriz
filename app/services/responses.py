5d51e2aa7da61f4440126856065f54926fea3e3d
Adrian Gizzi <adriangizzi@MacBook-Air-de-Adrian.local>
2026-05-18 13:40:03 -0300

    cambios responses

--- app/services/responses.py
+++ app/services/responses.py
@@ -1,9 +1,14 @@
+import pandas as pd
 from app.services.sheets import buscar_autos
 
 def generar_respuesta(texto):
 
-    texto = texto.lower() # Esto realiza el pasado automatico de cualquier saludo este bien o mal escrito 
 
+
+
+
+    texto = texto.lower() # Esto realiza el pasado automatico de cualquier saludo este bien o mal escrito 
+    
     # =========================
     # SALUDOS
     # =========================
@@ -14,34 +19,37 @@ def generar_respuesta(texto):
             "Hola 👋 Soy el asistente virtual de Zabaleo Motors 🚗\n"
             "¿Qué vehículo estás buscando?"
         )
-
+    
     # =========================
     # BUSCAR AUTOS
     # =========================
-
+    
     autos = buscar_autos(texto)
-
+            
     if len(autos) > 0:
-
+        
         respuesta = "Encontré estos vehículos 🚗\n\n"
-
+    
         for _, auto in autos.iterrows():
 
-            respuesta += (
-                f"🚗 {auto['marca']} {auto['modelo']}\n"
-                f"📅 Año: {int(auto['año'])}\n"
-                f"💵 Precio: {int(auto['precio'])}\n"
-                f"⚙️ Transmisión: {auto['transmisión']}\n"
-                f"⛽ Combustible: {auto['combustible']}\n"
-                f"🛣️ KM: {int(auto['km'])}\n\n"# con int convierto un float en un entero , le saca los decimales 
-                #f"🏢 Agencia: {auto['agencia']}\n\n"
-            )
-        respuesta += (
+            anio = auto['año']
+            precio = auto['precio']
+            km = auto['km']
+
+    respuesta += (
+        f"🚗 {auto['marca']} {auto['modelo']}\n"
+        f"📅 Año: {int(anio) if pd.notna(anio) else 'N/D'}\n"
+        f"💵 Precio: {int(precio) if pd.notna(precio) else 'Consultar'}\n"
+        f"⚙️ Transmisión: {auto['transmisión']}\n"
+        f"⛽ Combustible: {auto['combustible']}\n"
+        f"🛣️ KM: {int(km) if pd.notna(km) else 'N/D'}\n\n" # pd es solo para confirma que el valor existe 
+    )
+    respuesta += (
             "\n👉 ¿Cuál te interesa más?\n"
             "Puedo ayudarte con financiación, permutas o más fotos."
         )
 
-        return respuesta
+    return respuesta
 
     # =========================
     # DEFAULT
