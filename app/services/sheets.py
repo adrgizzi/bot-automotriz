0539be54c49136afe93c63f6ac0fe3afd5ac5837
Adrian Gizzi <adriangizzi@MacBook-Air-de-Adrian.local>
2026-05-18 13:50:39 -0300

    cambios sheets

--- app/services/sheets.py
+++ app/services/sheets.py
@@ -2,7 +2,15 @@ import pandas as pd
 
 SHEET_URL = "https://docs.google.com/spreadsheets/d/1fEmcM4fzV2TwmzyZVqF9yS2mxH305CarUKURpC4t1xo/export?format=csv"
 
-def buscar_autos(query):
+def buscar_autos(texto):
+
     df = pd.read_csv(SHEET_URL)
-    mascara = df.apply(lambda row: row.astype(str).str.contains(query, case=False).any(), axis=1)
-    return df[mascara]
+
+    texto = texto.lower()
+
+    resultados = df[
+        df["marca"].str.lower().str.contains(texto, na=False) |
+        df["modelo"].str.lower().str.contains(texto, na=False)
+    ]
+
+    return resultados
