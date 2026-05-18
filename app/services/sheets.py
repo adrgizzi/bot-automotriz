1242b6ad130ec1b180727ca35f3c9c4941b69eeb
Adrian Gizzi <adriangizzi@MacBook-Air-de-Adrian.local>
2026-05-18 15:32:29 -0300

    cambios sheets

--- app/services/sheets.py
+++ app/services/sheets.py
@@ -1,19 +1,10 @@
 import pandas as pd
 
-
 SHEET_URL = "https://docs.google.com/spreadsheets/d/1fEmcM4fzV2TwmzyZVqF9yS2mxH305CarUKURpC4t1xo/export?format=csv"
 
 def buscar_autos(query):
     df = pd.read_csv(SHEET_URL)
-    
-    # Eliminar filas donde marca está vacía
-    df = df.dropna(subset=['marca'])
-    
-    df = df.rename(columns={
-        'transmision': 'transmisión'
-    })
-    
+    print("COLUMNAS EXACTAS:", list(df.columns))
     mascara = df.apply(lambda row: row.astype(str).str.contains(query, case=False).any(), axis=1)
     return df[mascara]
-
 
