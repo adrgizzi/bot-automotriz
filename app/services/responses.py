7c6d2e15783d2d1c9ea5daea06ef9bdc3ed8c4db
Adrian Gizzi <adriangizzi@MacBook-Air-de-Adrian.local>
2026-05-18 12:55:43 -0300

    cambios responses

--- app/services/responses.py
+++ app/services/responses.py
@@ -2,35 +2,50 @@ from app.services.sheets import buscar_autos
 
 def generar_respuesta(texto):
 
+    texto = texto.lower()
+
+    # =========================
+    # SALUDOS
+    # =========================
+
+    if "hola" in texto or "buenas" in texto:
+
+        return (
+            "Hola 👋 Soy el asistente virtual de Zabaleo Motors 🚗\n"
+            "¿Qué vehículo estás buscando?"
+        )
+
+    # =========================
+    # BUSCAR AUTOS
+    # =========================
+
     autos = buscar_autos(texto)
-    
-    
 
     if len(autos) > 0:
-        if "hola" in texto or "buenas" in texto:
-            return (
-        "Hola 👋 Soy el asistente virtual de Zabaleo Motors 🚗\n"
-        "¿Qué vehículo estás buscando?"
-)
 
         respuesta = "Encontré estos vehículos 🚗\n\n"
 
         for _, auto in autos.iterrows():
 
             respuesta += (
-             f"🚗 {auto['marca']} {auto['modelo']}\n"
-            f"📅 Año: {auto['año']}\n"
-            f"💵 Precio: {auto['precio']}\n"
-            f"⚙️ Transmisión: {auto['transmisión']}\n"
-            f"⛽ Combustible: {auto['combustible']}\n"
-            f"🛣️ KM: {auto['km']}\n" 
-            f"🏢 Agencia: {auto['agencia']}\n\n"# con el doble separa modelo a modelo 
-            
-)
-            respuesta += (
-             "\n👉 ¿Cuál te interesa más?\n"
+                f"🚗 {auto['marca']} {auto['modelo']}\n"
+                f"📅 Año: {auto['año']}\n"
+                f"💵 Precio: {auto['precio']}\n"
+                f"⚙️ Transmisión: {auto['transmisión']}\n"
+                f"⛽ Combustible: {auto['combustible']}\n"
+                f"🛣️ KM: {auto['km']}\n"
+                f"🏢 Agencia: {auto['agencia']}\n\n"
+            )
+
+        respuesta += (
+            "\n👉 ¿Cuál te interesa más?\n"
             "Puedo ayudarte con financiación, permutas o más fotos."
-)
+        )
+
         return respuesta
+
+    # =========================
+    # DEFAULT
+    # =========================
 
     return "No encontré vehículos con esa búsqueda 😕"
