3ff64c7716ea0de4034bf8b672cf4ff1ce8580ea
Adrian Gizzi <adriangizzi@MacBook-Air-de-Adrian.local>
2026-05-18 14:08:16 -0300

    cambios responses

--- app/services/responses.py
+++ app/services/responses.py
@@ -1,14 +1,11 @@
 import pandas as pd
 from app.services.sheets import buscar_autos
 
+
 def generar_respuesta(texto):
 
+    texto = texto.lower()
 
-
-
-
-    texto = texto.lower() # Esto realiza el pasado automatico de cualquier saludo este bien o mal escrito 
-    
     # =========================
     # SALUDOS
     # =========================
@@ -19,45 +16,67 @@ def generar_respuesta(texto):
             "Hola 👋 Soy el asistente virtual de Zabaleo Motors 🚗\n"
             "¿Qué vehículo estás buscando?"
         )
-    
+
     # =========================
     # BUSCAR AUTOS
     # =========================
-    
+
     autos = buscar_autos(texto)
-            
+
     if len(autos) > 0:
-        
+
         respuesta = "Encontré estos vehículos 🚗\n\n"
-    
+
         for _, auto in autos.iterrows():
 
-            anio = auto['año']
+            # ==========
+            # LIMPIAR PRECIO
+            # ==========
+
             precio = auto['precio']
+
+            if pd.notna(precio):
+
+                precio = str(precio)
+                precio = precio.replace("$", "")
+                precio = precio.replace(".", "")
+                precio = precio.strip()
+
+            else:
+                precio = "Consultar"
+
+            # ==========
+            # VARIABLES
+            # ==========
+
+            anio = auto['año']
             km = auto['km']
 
-    respuesta += (
-        f"🚗 {auto['marca']} {auto['modelo']}\n"
-        f"📅 Año: {int(anio) if pd.notna(anio) else 'N/D'}\n"
-        f"💵 Precio: {int(precio) if pd.notna(precio) else 'Consultar'}\n"
-        f"⚙️ Transmisión: {auto['transmisión']}\n"
-        f"⛽ Combustible: {auto['combustible']}\n"
-        f"🛣️ KM: {int(km) if pd.notna(km) else 'N/D'}\n\n" # pd es solo para confirma que el valor existe 
-    )
-    respuesta += (
+            # ==========
+            # RESPUESTA
+            # ==========
+
+            respuesta += (
+                f"🚗 {auto['marca']} {auto['modelo']}\n"
+                f"📅 Año: {int(anio) if pd.notna(anio) else 'N/D'}\n"
+                f"💵 Precio: {precio}\n"
+                f"⚙️ Transmisión: {auto['transmisión']}\n"
+                f"⛽ Combustible: {auto['combustible']}\n"
+                f"🛣️ KM: {int(km) if pd.notna(km) else 'N/D'}\n\n"
+            )
+
+        respuesta += (
             "\n👉 ¿Cuál te interesa más?\n"
             "Puedo ayudarte con financiación, permutas o más fotos."
         )
 
-    return respuesta
+        return respuesta
 
     # =========================
     # DEFAULT
     # =========================
 
     return "No encontré vehículos con esa búsqueda 😕"
-
-
 #Primero:
 #saludos
 #intenciones
